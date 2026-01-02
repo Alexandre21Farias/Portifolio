206e2502f8f631c01427894ed180fbe638aab0ce
Alexandre Lima Farias <alexandre21farias@gmail.com>
2026-01-02 18:45:25 -0300

    add my social networks

--- src/PageWeb/hello/Hello.jsx
+++ src/PageWeb/hello/Hello.jsx
@@ -1,5 +1,6 @@
 import { useIntersectionObserver } from "/src/hooks/useIntersectionObserver.js";
 import styles from "./styles.module.css";
+import { FaGithub, FaLinkedin, FaInstagram, FaEnvelope } from "react-icons/fa";
 
 export default function HalloComponent() {
   const { ref, isVisible } = useIntersectionObserver({
@@ -20,11 +21,13 @@ export default function HalloComponent() {
         <span className={styles.Greeting}>
           <h1>
             Hi, i am{" "}
-            <span style={{ color: "var(--text-muted)" }}>Alexandre F.</span>
+            <span style={{ color: "var(--orange-light)" }}>
+              Alexandre Farias
+            </span>
           </h1>
 
           <h1>
-            a <span style={{ color: "var(--text-muted)" }}>Full-Stack</span>{" "}
+            a <span style={{ color: "var(--orange-light)" }}>Full-Stack</span>{" "}
             developer.
           </h1>
         </span>
@@ -32,9 +35,36 @@ export default function HalloComponent() {
         <div
           className={`${styles.contact} ${isVisible ? styles.showContact : ""}`}
         >
-          <span>
-            <h1>TESTE</h1>
-          </span>
+          <a
+            href="https://github.com/Alexandre21Farias"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="GitHub"
+          >
+            <FaGithub />
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/alexandre-farias-774420206/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="LinkedIn"
+          >
+            <FaLinkedin />
+          </a>
+
+          <a
+            href="https://www.instagram.com/alex_xyz21/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Instagram"
+          >
+            <FaInstagram />
+          </a>
+
+          <a href="mailto:alexandre21farias@gmail.com" aria-label="Email">
+            <FaEnvelope />
+          </a>
         </div>
       </div>
     </section>
